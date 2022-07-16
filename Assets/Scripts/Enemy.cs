9daf1939aebe2add2e0f9e3e8d0e6c2cb2b63aac
Alex Vinogradov <yarpoplar@gmail.com>
2022-07-16 14:05:46 +0300

    Added sprite to enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,15 +6,28 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour, IDamageable
 {
     private NavMeshAgent navMeshAgent;
+    public GameObject spriteRoot;
+    public GameObject sprite;
     public float Health = 5;
     void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        if ((!spriteRoot) || (sprite))
+        {
+            Debug.LogError("Set sprite/spriteRoot for enemy!");
+        }
+        //navMeshAgent.velocity
     }
 
     private void Update()
     {
         navMeshAgent.destination = GameManager.Instance.Player.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        //sprite.transform.LookAt(Camera.main.transform.position, -Vector3.up);
+        spriteRoot.transform.forward = Camera.main.transform.forward;
     }
 
     public void ApplyDamage(float damage)
