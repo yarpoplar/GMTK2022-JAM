2a09afa0a44078b6b6b5c3c8b4c9fcd85f38a004
Alex Vinogradov <yarpoplar@gmail.com>
2022-07-16 16:10:34 +0300

    Added glow to enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using DG.Tweening;
 
 enum AnimationState { Idle, Walk, Hit, Dead };
 
@@ -10,32 +11,48 @@ public class Enemy : MonoBehaviour, IDamageable
     private NavMeshAgent navMeshAgent;
     public GameObject spriteRoot;
     public GameObject sprite;
+    private Animator animator;
+    private Material spriteMat;
+    private Tween hitTween;
     public float Health = 5;
     void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
         if ((!spriteRoot) || (!sprite))
         {
             Debug.LogError("Set sprite/spriteRoot for enemy!");
         }
         //navMeshAgent.velocity
+        spriteMat = sprite.GetComponent<SpriteRenderer>().material;
     }
 
     private void Update()
     {
         navMeshAgent.destination = GameManager.Instance.Player.transform.position;
+        animator.SetFloat("Speed", navMeshAgent.velocity.magnitude);
     }
 
     private void LateUpdate()
     {
-        //sprite.transform.LookAt(Camera.main.transform.position, -Vector3.up);
         spriteRoot.transform.forward = Camera.main.transform.forward;
     }
 
     public void ApplyDamage(float damage)
     {
         Health -= damage;
+        spriteMat.SetFloat("_Hit", 1);
+        hitTween.Kill();
+        StartCoroutine(LateHitGlow());
+        float testFloat = 0;
+        DOTween.To(() => testFloat, x => testFloat = x, 52, 1);
         if (Health <= 0)
             Destroy(gameObject);
+    }
+
+    IEnumerator LateHitGlow()
+    {
+        yield return new WaitForSeconds(0.1f);
+        hitTween = spriteMat.DOFloat(0, "_Hit", 0.15f);
     }
 }
