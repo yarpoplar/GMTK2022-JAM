9fd7749b707ea3e59eda657a460d7f75fde7525a
Peppery <64744564+PepperyIcecream@users.noreply.github.com>
2022-07-16 16:34:36 +0300

    - [ADD] PlayerDash

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
+    [Header("Parameters")]
     [SerializeField]
     private float moveSpeed = 15f;
+    [SerializeField]
+    private float dashCooldown = 1f;
+    [SerializeField]
+    private float dashTime = .2f;
+    [SerializeField]
+    private float dashVelocity = 1200f;
     private Rigidbody rb = null;
+    [SerializeField]
+    private GameObject gfx;
 
     private Vector3 moveInput;
     private Vector3 moveVelocity;
+
+    private bool isDashing = false;
+    private bool canDash = true;
 
     void Start()
     {
@@ -18,9 +31,42 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        // Movement and look
+
+        if (isDashing)
+            return;
+
+        // Movement
         moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        moveVelocity = moveInput * moveSpeed;
+		moveVelocity = moveInput * moveSpeed;
+
+        if (Input.GetKeyDown(KeyCode.Space) && moveInput != Vector3.zero && canDash)
+            StartCoroutine(DashRoutine(moveInput));
+	}
+
+    private IEnumerator DashRoutine(Vector3 direction)
+	{
+        isDashing = true;
+        canDash = false;
+        float timeLeft = 0f;
+
+        gfx.transform.DOScale(new Vector3(.5f, .5f, .5f), dashTime / 2);
+
+        while (timeLeft <= dashTime)
+		{
+            timeLeft += Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+            rb.AddForce(direction * dashVelocity * Time.fixedDeltaTime, ForceMode.VelocityChange);
+
+            if (Mathf.Round(timeLeft) == Mathf.Round(dashTime / 2))
+                gfx.transform.DOScale(new Vector3(1, 1, 1), dashTime / 2);
+        }
+
+        isDashing = false;
+
+        yield return new WaitForSeconds(dashCooldown - dashTime);
+
+        canDash = true;
+
     }
 
 	private void FixedUpdate()
