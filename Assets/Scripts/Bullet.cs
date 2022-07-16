ed66c1815a0c46191c973a127f46bc44f0bbfdec
Peppery <64744564+PepperyIcecream@users.noreply.github.com>
2022-07-16 14:00:34 +0300

    -[UPD] Shooting

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField]
-    private float bulletForce = 500f;
+    private float Speed = 4000f;
+    [SerializeField]
+    private float Damage = 1;
     [Space]
     [SerializeField]
     private Rigidbody rb;
 
     void Start()
     {
-        rb.AddForce(transform.forward * bulletForce, ForceMode.Acceleration);
+        rb.AddForce(transform.forward * Speed, ForceMode.Acceleration);
     }
 
 	private void OnTriggerEnter(Collider other)
 	{
+        if (other.TryGetComponent(out IDamageable enemy))
+            enemy.ApplyDamage(Damage);
+
         Destroy(gameObject);	
 	}
 }
